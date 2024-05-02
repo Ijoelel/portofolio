eea611546d3c3b6c309ea315ae67b3ec033fd205
Ijoelel <fianarnatha13@gmail.com>
2024-05-02 11:06:57 +0700

    Update api,json

--- src/assets/Pages/Cert.jsx
+++ src/assets/Pages/Cert.jsx
@@ -1,9 +1,44 @@
 import { Link } from "react-router-dom";
-import api from "../api.json";
+import json_api from "../api.json";
 import { Slider } from "reactjs-simple-slider";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import {
+	ChevronLeftIcon,
+	ChevronRightIcon,
+	ChevronUpIcon,
+	ChevronDownIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export default function Cert() {
+	const [api, setApi] = useState(json_api.result);
+
+	const sortByTitle = (e) => {
+		let newApi = api;
+		newApi.sort((a, b) => {
+			const nameA = a.title.toUpperCase(); // ignore upper and lowercase
+			const nameB = b.title.toUpperCase(); // ignore upper and lowercase
+			if (nameA < nameB) {
+				if (e.target.checked) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+			if (nameA > nameB) {
+				if (e.target.checked) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+
+			return 0;
+		});
+		setApi(newApi);
+	};
+	const sortByDate = () => {};
+	const sortByFrom = () => {};
+
 	return (
 		<div className="w-screen h-screen flex justify-center items-center flex-col gap-4 md:justify-center md:mt-0 md:flex-row">
 			<Link to="/" className="flex gap-3 absolute top-5 left-8">
@@ -32,14 +67,66 @@ export default function Cert() {
 						<thead>
 							<tr>
 								<th></th>
-								<th className="text-base">Title</th>
-								<th className="text-base">Date</th>
-								<th className="text-base">From</th>
+								<th className="text-base">
+									<div className="flex justify-center items-center gap-2">
+										<label
+											htmlFor="swap"
+											className="cursor-pointer"
+										>
+											Title
+										</label>
+										<label className="swap">
+											<input
+												id="swap"
+												type="checkbox"
+												onChange={sortByTitle}
+											/>
+
+											<ChevronUpIcon className="swap-on w-4" />
+
+											<ChevronDownIcon className="swap-off w-4" />
+										</label>
+									</div>
+								</th>
+								<th className="text-base">
+									<div className="flex justify-center items-center gap-2">
+										<label
+											htmlFor="swap"
+											className="cursor-pointer"
+										>
+											Date
+										</label>
+										<label className="swap">
+											<input id="swap" type="checkbox" />
+
+											<ChevronUpIcon className="swap-on w-4" />
+
+											<ChevronDownIcon className="swap-off w-4" />
+										</label>
+									</div>
+								</th>
+								<th className="text-base">
+									<div className="flex justify-center items-center gap-2">
+										<label
+											htmlFor="swap"
+											className="cursor-pointer"
+										>
+											From
+										</label>
+										<label className="swap">
+											<input id="swap" type="checkbox" />
+
+											<ChevronUpIcon className="swap-on w-4" />
+
+											<ChevronDownIcon className="swap-off w-4" />
+										</label>
+									</div>
+								</th>
 								<th></th>
 							</tr>
 						</thead>
 						<tbody>
-							{api.result.map((item, i) => {
+							{api.map((item, i) => {
 								return (
 									<tr key={i}>
 										<th>{i + 1}</th>
@@ -61,46 +148,48 @@ export default function Cert() {
 												onClick={() =>
 													document
 														.getElementById(
-															`my_modal_${i}`,
+															`my_modal_${i}`
 														)
 														.showModal()
 												}
 											>
 												Show
 											</button>
-										</td>
-										<dialog
-											id={`my_modal_${i}`}
-											className="modal"
-										>
-											<div className="modal-box">
-												{item.image.length > 1 ? (
-													<Slider
-														objectFit="contain"
-														images={item.image}
-														leftArrow={() => {
-															return (
-																<ChevronLeftIcon className="w-7 h-7 md:w-12 stroke-accent opacity-50 hover:opacity-100" />
-															);
-														}}
-														rightArrow={() => {
-															return (
-																<ChevronRightIcon className="w-7 h-7 md:w-12 stroke-accent opacity-50 hover:opacity-100" />
-															);
-														}}
-													/>
-												) : (
-													<img src={item.image[0]} />
-												)}
-											</div>
-
-											<form
-												method="dialog"
-												className="modal-backdrop"
+											<dialog
+												id={`my_modal_${i}`}
+												className="modal"
 											>
-												<button>close</button>
-											</form>
-										</dialog>
+												<div className="modal-box">
+													{item.image.length > 1 ? (
+														<Slider
+															objectFit="contain"
+															images={item.image}
+															leftArrow={() => {
+																return (
+																	<ChevronLeftIcon className="w-7 h-7 md:w-12 stroke-accent opacity-50 hover:opacity-100" />
+																);
+															}}
+															rightArrow={() => {
+																return (
+																	<ChevronRightIcon className="w-7 h-7 md:w-12 stroke-accent opacity-50 hover:opacity-100" />
+																);
+															}}
+														/>
+													) : (
+														<img
+															src={item.image[0]}
+														/>
+													)}
+												</div>
+
+												<form
+													method="dialog"
+													className="modal-backdrop"
+												>
+													<button>close</button>
+												</form>
+											</dialog>
+										</td>
 									</tr>
 								);
 							})}
